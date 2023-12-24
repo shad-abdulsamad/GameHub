c5ffe289171ba57d2e5b35cdcdeb8a5392f523c6
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-12-24 23:47:12 +0300

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,7 +11,8 @@ export interface Platform{
   id: number;
   name: string;
   background_image:string,
-  parent_platforms: {platform:Platform}[]
+  parent_platforms: {platform:Platform}[],
+  metacritic: number
 }
 
 interface FetchGamesResponse {
