e2aa8b83de32580f4e77880fcb776d4af405920e
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-12-27 00:09:28 +0300

    implemented searching functionality

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,7 +16,7 @@ export interface Platform{
 }
 
  
-const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder}}, [gameQuery]);
+const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}}, [gameQuery]);
  
 
 export default useGames;
