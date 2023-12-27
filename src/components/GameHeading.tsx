6add4247989d5c48fedce6ffd8cec807928dc373
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-12-27 16:28:48 +0300

    dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,19 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  const headings = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
+      {headings}
+    </Heading>
+  );
+};
+
+export default GameHeading;
